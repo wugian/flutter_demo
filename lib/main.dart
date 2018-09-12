9d50e95ce6f06f6aa68ff88a26f3da07d3fd91c5
wugian <wugian@qq.com>
2018-09-13 00:25:42 +0800

    data

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,131 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:wugian_flutter/video_player_lee.dart';
-//import 'package:video_player/video_player.dart';
 
 //void main() => runApp(new VideoApp("url"));
+//void main() => runApp(new PT());
 void main() => runApp(new MyApp());
+
+class PT extends StatefulWidget {
+  @override
+  _PT createState() => new _PT();
+}
+
+class _PT extends State<PT> {
+  List data;
+
+  void _httpClient() async {
+    var responseBody;
+
+    var httpClient = new HttpClient();
+    var request = await httpClient
+        .getUrl(Uri.parse("http://api.hclyz.cn:81/mf/json.txt"));
+
+    var response = await request.close();
+
+    if (response.statusCode == 200) {
+      responseBody = await response.transform(utf8.decoder).join();
+
+      var convertDataToJson = jsonDecode(responseBody)["pingtai"];
+      setState(() {
+        data = convertDataToJson;
+      });
+    } else {
+      print("error");
+    }
+  }
+
+  @override
+  void initState() {
+    _httpClient();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Center(
+      child: new GridView.count(
+        children: data != null ? _getItem() : _loading(),
+        crossAxisCount: 4,
+      ),
+    )
+//      body:
+        );
+  }
+
+  List<Widget> _loading() {
+    return <Widget>[
+      new Container(
+        height: 300.0,
+        child: new Center(
+            child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new CircularProgressIndicator(
+              strokeWidth: 1.0,
+            ),
+            new Text("正在加载"),
+          ],
+        )),
+      )
+    ];
+  }
+
+  List<Widget> _getItem() {
+    return data.map((item) {
+      return new Center(
+        child: new Padding(
+          padding: const EdgeInsets.all(1.0),
+          child: _getRowWidget(item),
+        ),
+//        elevation: 3.0,
+//        margin: const EdgeInsets.all(1.0),
+      );
+    }).toList();
+  }
+
+  Widget _getRowWidget(item) {
+    return new GestureDetector(
+        onTap: () {
+          print(item.toString());
+          Navigator.of(context).push(new MaterialPageRoute(builder: (_) {
+            return new MyHomePage(
+              title: item["address"],
+            );
+          }));
+        },
+        child: new Row(
+          children: <Widget>[
+            new Center(
+              child: new Stack(
+                children: <Widget>[
+//                  new Text("${item['title']}",),
+                  new FadeInImage.assetNetwork(
+                    placeholder:
+                        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536774923506&di=2a3e4d720505d32d4c569220856abb0d&imgtype=0&src=http%3A%2F%2F06.imgmini.eastday.com%2Fmobile%2F20180724%2F20180724224829_40f964a0b352223127c92a66e6b14054_3.jpeg",
+                    image: "${item['xinimg']}",
+                    width: 80.0,
+                    height: 80.0,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  new Positioned(
+                    left: 1.0,
+                    right: 1.0,
+                    bottom: 5.0,
+                    child: new Text(
+                      "${item['title']}",
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(fontSize: 14.0, color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -15,14 +134,12 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'WUGIAN'),
+      home: new PT(),
     );
   }
 }
-
-var id = 0;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -33,48 +150,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 100;
   List data;
-
-  void _pullNet() async {
-    await http
-        .get("http://www.wanandroid.com/project/list/1/json?cid=1")
-        .then((http.Response response) {
-      var convertDataToJson = JSON.decode(response.body);
-      convertDataToJson = convertDataToJson["data"]["datas"];
-
-      print(convertDataToJson);
-
-      setState(() {
-        data = convertDataToJson;
-      });
-      convertDataToJson.forEach((item) {
-        print(item["envelopePic"]);
-      });
-    });
-  }
 
   void _httpClient() async {
     var responseBody;
 
     var httpClient = new HttpClient();
-    var request = await httpClient.getUrl(
-        Uri.parse("http://www.wanandroid.com/project/list/1/json?cid=1"));
+    var request = await httpClient
+//        .getUrl(Uri.parse("http://api.hclyz.cn:81/mf/jsondaxiaojie.txt"));
+        .getUrl(Uri.parse("http://api.hclyz.cn:81/mf/" + widget.title));
 
     var response = await request.close();
 
     if (response.statusCode == 200) {
       responseBody = await response.transform(utf8.decoder).join();
 
-      var convertDataToJson = jsonDecode(responseBody)["data"]["datas"];
+      var convertDataToJson = jsonDecode(responseBody)["zhubo"];
       setState(() {
         data = convertDataToJson;
-      });
-
-      convertDataToJson.forEach((item) {
-        print("***********************");
-        print(item["envelopePic"]);
-        print("***********************");
       });
     } else {
       print("error");
@@ -83,26 +176,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    _pullNet();
-//    _httpClient();
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    _httpClient();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new ListView(children: data != null ? _getItem() : _loading()),
+      body: new GridView.count(
+        children: data != null ? _getItem() : _loading(),
+        crossAxisCount: 3,
+      ),
     );
   }
 
@@ -128,7 +211,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return data.map((item) {
       return new Card(
         child: new Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(1.0),
           child: _getRowWidget(item),
         ),
         elevation: 3.0,
@@ -141,143 +224,56 @@ class _MyHomePageState extends State<MyHomePage> {
     return new GestureDetector(
         onTap: () {
           print(item.toString());
-//          Navigator.of(context).push(new MaterialPageRoute(builder: (_) {
-//            return new SecondPage(title: data["envelopePic"]);
-//          }));
-          id = (id == 0 ? 1 : 0);
           Navigator.of(context).push(new MaterialPageRoute(builder: (_) {
-//            return new VideoApp(data[0]["envelopePic"]);
             return new SecondPage(
-              title: data[0]["envelopePic"],
+              title: item["address"],
             );
           }));
         },
         child: new Row(
           children: <Widget>[
-            new Flexible(
-                flex: 1,
-                fit: FlexFit.tight, //和android的weight=1效果一样
-                child: new Stack(
-                  children: <Widget>[
-                    new Column(
-                      children: <Widget>[
-                        new Text("${item["title"]}".trim(),
-                            style: new TextStyle(
-                              color: Colors.black,
-                              fontSize: 20.0,
-                            ),
-                            textAlign: TextAlign.left),
-                        new Text(
-                          "${item["desc"]}",
-                          maxLines: 3,
-                        )
-                      ],
+            new Center(
+              child: new Stack(
+                children: <Widget>[
+//                  new Text("${item['title']}",),
+                  new FadeInImage.assetNetwork(
+                    placeholder:
+                        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536774923506&di=2a3e4d720505d32d4c569220856abb0d&imgtype=0&src=http%3A%2F%2F06.imgmini.eastday.com%2Fmobile%2F20180724%2F20180724224829_40f964a0b352223127c92a66e6b14054_3.jpeg",
+                    image: "${item['img']}",
+                    width: 114.0,
+                    height: 100.0,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  new Positioned(
+                    left: 1.0,
+                    right: 1.0,
+                    bottom: 5.0,
+                    child: new Text(
+                      "${item['title']}",
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(fontSize: 14.0, color: Colors.blue),
                     ),
-                  ],
-                )),
-            new ClipRect(
-              child: new FadeInImage.assetNetwork(
-                placeholder: "images/ic_shop_normal.png",
-                image: "${item['envelopePic']}",
-                width: 50.0,
-                height: 50.0,
-                fit: BoxFit.fitWidth,
+                  ),
+                ],
               ),
-            ),
+            )
           ],
         ));
-
-//    return
+//        child: new Row(
+//          children: <Widget>[
+//            new Center(
+//              child: new FadeInImage.assetNetwork(
+//                placeholder:
+//                    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536774923506&di=2a3e4d720505d32d4c569220856abb0d&imgtype=0&src=http%3A%2F%2F06.imgmini.eastday.com%2Fmobile%2F20180724%2F20180724224829_40f964a0b352223127c92a66e6b14054_3.jpeg",
+//                image: "${item['img']}",
+//                width: 90.0,
+//                height: 90.0,
+//                fit: BoxFit.fitWidth,
+//              ),
+//            ),
+//          ],
+//        ));
   }
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        title: new Text(widget.title),
-//      ),
-//      body: new ListView.builder(
-//        itemCount: data.length,
-//        itemBuilder: (context, index) {
-//          return new ListTile(
-//            title: new Text('${data[index]["desc"]}'),
-//          );
-//        },
-//      ),
-////      body: new Center(
-////
-////        child: new ListView(
-//////          mainAxisAlignment: MainAxisAlignment.center,
-////          padding: new EdgeInsets.all(10.0),
-////          children: <Widget>[
-////            new Center(
-////              child: new Text(
-////                'You have pushed the button this many times:',
-////              ),
-////            ),
-////            new Center(
-////              child: new Text(
-////                '$_counter',
-////                style: Theme.of(context).textTheme.display1,
-////              ),
-////            ),
-////            new Container(
-////              padding: new EdgeInsets.only(top: 10.0),
-////              width: 86.0,
-////              height: 48.0,
-////              child: new MaterialButton(
-////                elevation: 12.0,
-////                color: Colors.greenAccent,
-////                splashColor: Colors.green,
-////                onPressed: _decrementCounter,
-////                child: new Text("minus"),
-////              ),
-////            ),
-////            new Container(
-////              padding: new EdgeInsets.only(top: 10.0),
-////              width: 86.0,
-////              height: 48.0,
-////              child: new MaterialButton(
-////                elevation: 12.0,
-////                minWidth: 80.0,
-////                color: Colors.greenAccent,
-////                splashColor: Colors.green,
-////                onPressed: _incrementCounter,
-////                child: new Text("add"),
-////              ),
-////            ),
-////            new Container(
-////              padding: new EdgeInsets.only(top: 10.0),
-////              child: new Image.network(
-////                "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3573075543,1312702639&fm=27&gp=0.jpg",
-////                width: 400.0,
-////                height: 400.0,
-////              ),
-////            ),
-////            new Container(
-//////                child: new ListView.builder(
-//////              itemCount: items.length,
-//////              itemBuilder: (context, index) {
-//////                return new ListTile(
-//////                  title: new Text('${items[index]}'),
-//////                );
-//////              },
-//////            )
-////                ),
-//
-////            new ListView.builder(itemBuilder: (context, index) {
-////              return new ListTile(title: new Text("bbbb"));
-////            })
-////      ],
-////    ),)
-////    ,
-//
-////      floatingActionButton: new FloatingActionButton(
-////        onPressed: _incrementCounter,
-////        tooltip: 'Increment',
-////        child: new Icon(Icons.exposure_neg_1),
-////      ), // This trailing comma makes auto-formatting nicer for build methods.
-//    );
-//  }
 }
 
 class SecondPage extends StatefulWidget {
@@ -298,16 +294,7 @@ class SecondState extends State<SecondPage> {
   @override
   void initState() {
     super.initState();
-
-    print("════════════════════════");
-    print('${widget.title != null ? widget.title : ''}');
-    print("════════════════════════");
-    id = 0;
-    _controller = VideoPlayerController.network(
-      id == 0
-          ? 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_20mb.mp4'
-          : 'rtmp://live.hkstv.hk.lxdns.com/live/hks',
-    )
+    _controller = VideoPlayerController.network(widget.title)
       ..addListener(() {
         final bool isPlaying = _controller.value.isPlaying;
         if (isPlaying != _isPlaying) {
@@ -317,10 +304,16 @@ class SecondState extends State<SecondPage> {
         }
       })
       ..initialize().then((_) {
-//        _controller.play;
+        _controller.play();
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _controller = null;
   }
 
   @override
@@ -340,8 +333,9 @@ class SecondState extends State<SecondPage> {
           onPressed: _controller.value.isPlaying
               ? _controller.pause
               : _controller.play,
+//        onPressed: widget.dispose(),
           child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+            Icons.arrow_back,
           ),
         ),
       ),
